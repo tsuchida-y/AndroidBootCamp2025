955cfe899a1d20a360c16449a219f8dd8ad8bea2
tsuchida <yu41926t.tutida@icloud.com>
2025-10-07 18:28:15 +0900

    chore:テストコード用の依存関係の追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,14 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.robolectric)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
 }
